72958980e484201c3da4b65204b661dac393a30e
Victorkaffekande <90904666+Victorkaffekande@users.noreply.github.com>
2022-11-16 10:03:22 +0100

    OnionSetup

---
+++ Api/Controllers/fillerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+public class fillerController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return null;
+    }
+}
