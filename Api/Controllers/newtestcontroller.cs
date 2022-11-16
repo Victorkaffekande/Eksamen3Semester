16a3e357eca020916a5525c6ac878c19728fc288
Victorkaffekande <90904666+Victorkaffekande@users.noreply.github.com>
2022-11-16 10:40:04 +0100

    Create newtestcontroller.cs

---
+++ Api/Controllers/newtestcontroller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+public class newtestcontroller : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return null;
+    }
+}
