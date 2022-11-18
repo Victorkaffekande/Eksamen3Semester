afb29bb0e0f2f902a8d3495bf8d7319efb6eeefd
Victorkaffekande <90904666+Victorkaffekande@users.noreply.github.com>
2022-11-18 10:35:45 +0100

    Domain enteties setup
    Co-Authored-By: magn20 <71693106+magn20@users.noreply.github.com>

---
+++ Domain/Enteties/Post.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain;
+
+public class Post
+{
+    public int Id { get; set; }
+    public int ProjectId { get; set; }
+    public String Description { get; set; }
+    public DateTime PostDate { get; set; }
+    public String Image { get; set; }
+}
