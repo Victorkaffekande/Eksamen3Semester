38acf27277b36a83111e3f93e01e8493a2f10387
magn20 <magnum.overgaar@hotmail.com>
2022-12-02 09:25:04 +0100

    validator

---
+++ Backend/Application/Validators/UserDTOValidator.cs
@@ -0,0 +1,21 @@
+﻿using Application.DTOs;
+using FluentValidation;
+
+namespace Application.Validators;
+
+public class UserDTOValidator : AbstractValidator<UserDTO>
+{
+    public UserDTOValidator()
+    {
+        RuleFor(u => u.Id).GreaterThan(0).WithState(x =>throw new ArgumentException("id cannot be below 1"));
+        RuleFor(u => u.Username).NotNull().NotEmpty();
+        RuleFor(u => u.BirthDay).Must(BeValidDate);
+        
+        RuleFor(u => u.Email).EmailAddress();
+    }
+    
+    private bool BeValidDate(DateOnly dateOnly)
+    {
+        return !dateOnly.Equals(default(DateOnly));
+    }
+}
