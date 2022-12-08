79f216960a123633b42035f5f77b65ab4baac96e
Victorkaffekande <90904666+Victorkaffekande@users.noreply.github.com>
2022-12-08 20:07:47 +0100

    dashboard work in progress

--- Frontend/src/app/dashboard/dashboard.component.ts
+++ Frontend/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {LikeService} from "../../services/like.service";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-dashboard',
@@ -8,15 +10,39 @@ import {LikeService} from "../../services/like.service";
 })
 export class DashboardComponent implements OnInit {
   postList: any;
+  selectedPost: any;
 
-  constructor(private service: LikeService) { }
+  skip: number = 0;
+  take: number = 10;
+  batchSize: number = 10;
 
-  ngOnInit(): void {
-this.getPosts()
+  constructor(private service: LikeService,
+              private modalService: NgbModal,
+              private router: Router) {
   }
 
-  async getPosts(){
-    this.postList = await this.service.getAllPostByLikedUsers(3,0,10);
+  ngOnInit() {
+    this.getInitialPosts();
   }
 
+  async getInitialPosts() {
+    this.postList = await this.service.getAllPostByLikedUsers(2, this.skip, this.take);
+    this.skip += this.batchSize;
+    this.take += this.batchSize;
+  }
+
+  async getMorePosts() {
+    let a = await this.service.getAllPostByLikedUsers(2, 0, 10);
+    this.postList.push(a);
+  }
+
+  openModal(modal: any, p: any) {
+    this.selectedPost = p;
+    this.modalService.open(modal, {size: 'xl'});
+  }
+
+
+  goToProject(id: any) {
+    //TODO FIX VICTOR
+  }
 }
